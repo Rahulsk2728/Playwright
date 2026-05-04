03d3e840e64a2da9172b6776504be40bc8e6c1de
Santosh J S <santoshjs@MacBookPro.lan>
2026-05-04 16:26:13 +0530

    API code

--- tests/WebAPItest1.spec.js
+++ tests/WebAPItest1.spec.js
@@ -1,28 +1,45 @@
 import { test, expect, request } from '@playwright/test';
+
+
+//Payload storage 
 const loginPayLoad = { userEmail: "anshika@gmail.com", userPassword: "IamKing@00" };
-
-
+const orderPayload = {"orders": [{"country":"India","product0rderedId":"62023a761Sfcf72fe9dfc619"}]};
+let token;
 
 test.beforeAll(async () => {
+
+    //Login API 
     const apiContext = await request.newContext();
-    const loginResponse = await apiContext.post("https://rahulshettyacaemy.com/api/ecom/auth/login",
+    const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/com/auth/login",
 
         // Headers 
         {
             data: loginPayLoad
         })
-    // 200 , response code status code check 
+
+    // Debug (optional but useful)
+    console.log(await loginResponse.text());
+
+    // Status check
     expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = await loginResponseJson.json();
-    const token = loginResponseJson.token;
-    console.log(token);
 
-});
+    const loginResponseJson = await loginResponse.json();
+    token = loginResponseJson.token;
+    console.log("Token:", token);
 
+// Create order API
+  const orderResponse = await apiContext.post(
+    "https://rahulshettyacademy.com/api/ecom/order/create-order",
+    {
+      data: orderPayload,
+      headers: {
+        'Authorization': token,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
 
-testBeforeEach(() => {
-
-
+  console.log(await orderResponse.json());
 });
 
 
@@ -30,13 +47,20 @@ testBeforeEach(() => {
 /* First Playwright test locators  */
 test('First Playwright test', async ({ page }) => {
 
-    //To navigate to website
-    await page.goto("https://rahulshettyacademy.com/client");
 
-    //Loctaors
-    await page.locator("#userEmail").fill("anshika@gmail.com");
-    await page.locator("#userPassword").fill("Iamking@000");
-    await page.locator("[value='Login']").click();
+    page.addInitScript(value => {
+
+        window.localStorage.setItem('token', value);
+
+    }, value);
+
+    // //To navigate to website
+    // await page.goto("https://rahulshettyacademy.com/client");
+
+    // //Loctaors
+    // await page.locator("#userEmail").fill("anshika@gmail.com");
+    // await page.locator("#userPassword").fill("Iamking@000");
+    // await page.locator("[value='Login']").click();
 
     //Wait condition to get the conatains text
     await page.waitForLoadState('networkidle');
